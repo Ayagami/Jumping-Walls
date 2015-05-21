a0ab628680d884d7f14fb3c73e1a7c88ba0cc8ce
Lukas Martin <luckystrike9422@gmail.com>
2015-05-21 20:50:42 -0300

    Adding some files

---
+++ Assets/UnityAnalyticsIntegration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Cloud.Analytics;
+
+public class UnityAnalyticsIntegration : MonoBehaviour {
+	public string projectId = "032e2591-7be4-434c-9c6b-88c9030d3675";
+	// Use this for initialization
+	void Start () {
+		
+
+		UnityAnalytics.StartSDK (projectId);
+		Debug.Log ("UnityAnalytics.StartSDK");
+	}
+	
+}
