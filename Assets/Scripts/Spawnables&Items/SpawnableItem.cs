fc7ffeb5ab9966c9fc30417854c8c06261cc0f47
Lucas Martin <lmartin@cabiexperience.com>
2015-07-10 22:18:21 -0300

    Adding GravityScale and speed to objects.

--- Assets/Scripts/Spawnables&Items/SpawnableItem.cs
+++ Assets/Scripts/Spawnables&Items/SpawnableItem.cs
@@ -5,11 +5,20 @@ public class SpawnableItem : MonoBehaviour {
 	public float timeThatCanBeAlive = 10f;
 	protected float timeAlive = 0f;
 	public float damage = 25f;
-
-
 	protected Player player = null; // Referencia al player.
+	
+	public Rigidbody2D rb2d = null;
 	public virtual void OnEnable(){
 		timeAlive = 0;
+		if(rb2d){
+			rb2d.gravityScale = Mathf.Abs(0.1f + 0.1f * ( 1 / 50-(GameManager.instance.LevelNumber+1)));
+		}else{
+			rb2d = this.GetComponent<Rigidbody2D>();
+		}
+	}
+	
+	void Start(){
+		//rb2d = this.GetComponent<Rigidbody2D>();
 	}
 
 	public virtual void Update(){
